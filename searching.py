3c0ecc5966f16bf723146b8dcdb8c41f393105f9
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-08-16 22:17:00 +0200

    Add some examples of matching with pos and endpos

--- searching.py
+++ searching.py
@@ -21,6 +21,24 @@ class SearchingTests(unittest.TestCase):
 
         self.assertIsNotNone(pattern.match("  <HTML>", 2))
 
+        # string doesn't start at position given by *pos*
+        pattern = re.compile(r'^<HTML>')
+
+        self.assertIsNone(pattern.match("  <HTML>", 2))
+        # slicing gives back a new string starting at expected pos
+        self.assertIsNotNone(pattern.match("  <HTML>"[2:]))
+
+    def test_match_and_search_with_endpos(self):
+        pattern = re.compile(r'<HTML>')
+
+        self.assertIsNone(pattern.match("<HTML>"[:2]))
+
+        self.assertIsNone(pattern.match("<HTML>", 0, 2))
+
+        # slicing and endpos have the same effect!
+        pattern = re.compile(r'<HTML>$')
+        self.assertIsNotNone(pattern.match("<HTML> ", 0, 6))
+        self.assertIsNotNone(pattern.match("<HTML> "[:6]))        
 
 if __name__ == '__main__':
     unittest.main()   # pragma: no cover

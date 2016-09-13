dd705860bbaa10d9b8e4730bb2922e7aa3669fef
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-09-13 22:01:55 +0200

    Add some more examples

--- modifying.py
+++ modifying.py
@@ -9,6 +9,11 @@ class ModifyingTests(unittest.TestCase):
         result = re.split(r'\n', 'Beautiful is better than ugly.\nExplicit is better than implicit')
         self.assertEqual(['Beautiful is better than ugly.','Explicit is better than implicit'], result)
 
+    def test_split_regex(self):
+        pattern = re.compile(r"\W")
+        self.assertEqual(['Hello', 'world'], pattern.split("Hello world"))
+
+        self.assertEqual([' '], pattern.findall("Hello world"))
 
 if __name__ == '__main__':
     unittest.main()   # pragma: no cover

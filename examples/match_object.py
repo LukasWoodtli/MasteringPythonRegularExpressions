24acb18e0dd7d8418ce53c02bb12f589c38c236e
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-09-26 16:21:11 +0200

    Add examples for MatchObject

---
+++ examples/match_object.py
@@ -0,0 +1,41 @@
+import unittest
+
+import re
+
+
+class MatchObjectTests(unittest.TestCase):
+    # A MatchObject is returned every time when following functions are called:
+    # - match
+    # - search
+    # - finditer
+    def test_group(self):
+        pattern = re.compile(r"(\w+) (\w+)")
+        match = pattern.search("Hello world")
+
+        self.assertEqual("Hello world", match.group())
+
+        self.assertEqual("Hello world", match.group(0))
+
+        self.assertEqual("Hello", match.group(1))
+
+        self.assertEqual("world", match.group(2))
+
+        with self.assertRaises(IndexError):
+             match.group(3)
+            
+        self.assertEqual(("Hello world", "world"), match.group(0, 2))
+
+
+    def test_group_named(self):
+        pattern = re.compile(r"(?P<first>\w+) (?P<second>\w+)")
+        match = pattern.search("Hello world")
+
+        self.assertEqual("Hello", match.group('first'))
+        self.assertEqual("Hello", match.group(1))
+
+        self.assertEqual(("Hello world", "Hello", "world"), match.group(0, 'first', 2))
+
+
+if __name__ == '__main__':
+    unittest.main()   # pragma: no cover
+

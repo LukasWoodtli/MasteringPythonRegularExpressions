47b756ed07e19bec1590a0f1a158f2641ae12169
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-09-28 16:10:55 +0200

    Add example for groups() function

--- examples/match_object.py
+++ examples/match_object.py
@@ -35,6 +35,18 @@ class MatchObjectTests(unittest.TestCase):
 
         self.assertEqual(("Hello world", "Hello", "world"), match.group(0, 'first', 2))
 
+    def test_groups(self):
+        pattern = re.compile(r"(\w+) (\w+)")
+        match = pattern.search("Hello world")
+
+        self.assertEqual(("Hello", "world"), match.groups())
+
+    def test_groups_not_matching(self):
+        pattern = re.compile(r"(\w+) (\w+)?")
+        match = pattern.search("Hello ")
+
+        self.assertEqual(("Hello", "mundo"), match.groups("mundo"))
+        self.assertEqual(("Hello", None), match.groups())
 
 if __name__ == '__main__':
     unittest.main()   # pragma: no cover

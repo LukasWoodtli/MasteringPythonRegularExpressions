bb154dbd83ff4a4188561821f85c281d76646f07
Lukas Woodtli <woodtli.lukas@gmail.com>
2016-10-06 22:00:06 +0200

    Add more examples for look-ahead

--- examples/look_around.py
+++ examples/look_around.py
@@ -36,7 +36,6 @@ class LookAroundTests(unittest.TestCase):
         pattern = re.compile(r'John(?!\sSmith)')
         iter = pattern.finditer("I would rather go out with John McLane than with John Smith or John Bon Jovi")
 
-        self.assertEqual(2, iter.size())
 
         result = iter.next()
         self.assertEqual(27, result.start())
@@ -46,6 +45,27 @@ class LookAroundTests(unittest.TestCase):
         self.assertEqual(63, result.start())
         self.assertEqual(67, result.end())
 
+    def test_substitution(self):
+        pattern = re.compile(r'\d{1,3}')
+        self.assertEqual(['123', '455', '678', '90'],
+                         pattern.findall("The number is: 12345567890"))
+
+        pattern = re.compile(r'\d{1,3}(?=(\d{3})+(?!\d))')
+        iter = pattern.finditer('1234567890')
+
+        result = iter.next()
+        self.assertEqual(0, result.start())
+        self.assertEqual(1, result.end())
+
+        result = iter.next()
+        self.assertEqual(1, result.start())
+        self.assertEqual(4, result.end())
+
+        result = iter.next()
+        self.assertEqual(4, result.start())
+        self.assertEqual(7, result.end())
+
+        self.assertEqual('1,234,567,890', pattern.sub(r'\g<0>,', "1234567890"))
 
 if __name__ == '__main__':
     unittest.main()   # pragma: no cover
